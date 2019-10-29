d701789b3fd77ce185685e282ffe4426669e8a1b
sumanmandadapu <suman.mandadapu@gmail.com>
2019-10-29 16:03:55 +0400

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,9 +13,61 @@ export class AppComponent implements OnInit {
   defaultQuestion="color";
   answer='';
 
+  genders :string[] = ['Male','Female'];
+
+   user = {
+        username:'',
+        email: '',
+        secret:'',
+        questionanswer:'',
+        gender:''
+    };
+    submitted =false;
+
   ngOnInit() {}
 
+  onClickSet(){
+    const suggestName="Suman";
+
+    this.signInform.setValue({
+      userDatagroup : {
+        username: suggestName,
+        email: "suman.mandadapu@gmail.com"
+      },
+      secret:"color",
+      questionanswer:"black",
+      gender:"Female"
+    });
+  }
+
+  onClickPatch(){
+    const suggestName="Suggested User";
+
+    this.signInform.form.patchValue({
+      userDatagroup : {
+        username: "Suman Mandadapu",
+      },
+      secret:"city",
+      questionanswer:"KMM",
+      gender:"Male"
+    });
+  }
+
+  onResetForm(){
+
+  }
+  onResetFormValue(){
+
+  }
+
   onSubmit(form:NgForm) {
+    this.submitted=true;
     console.log(this.signInform);
+    this.user.username=this.signInform.value.userDataGroup.username;
+    this.user.email=this.signInform.value.userDataGroup.email;
+    this.user.secret=this.signInform.value.secret;
+    this.user.questionanswer=this.signInform.value.questionanswer;
+    this.user.gender=this.signInform.value.gender;
+
   }
 }

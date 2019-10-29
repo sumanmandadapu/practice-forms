5ee8a661f3f455b4b1f1cf9d8a71c39b0a56d04a
sumanmandadapu <suman.mandadapu@gmail.com>
2019-10-29 11:32:55 +0400

    5 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { NgForm } from "@angular/forms";
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent  {
-  name = 'Angular';
+export class AppComponent implements OnInit {
+  
+  @ViewChild("signInForm", { static: true })
+  signInform: NgForm;
+  defaultQuestion="color";
+  answer='';
+
+  ngOnInit() {}
+
+  onSubmit(form:NgForm) {
+    console.log(this.signInform);
+  }
 }
